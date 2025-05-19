d1261d3b0f16d5bb7dfd96b68c28e2b6e5256653
Veritas <mailoflyk@163.com>
2025-05-20 05:27:13 +0800

    judger

--- judger.cpp
+++ judger.cpp
@@ -2,5 +2,22 @@
 #include "Boom.h"
 using namespace std;
 int main(){
-  
+  default_random_engine e;
+  uniform_int_distribution<int> u(1,ANS_NUM);
+  e.seed(time(0));
+  int ansid = u(e);
+  char str[R*C];
+  freopen("AnswerList.txt", "r", stdin);
+  while(ansid--) scanf("%s", str);
+  fclose(stdin);
+  freopen("CON", "r", stdin);
+
+  char pos[10];
+  for(int i=0;i<14;++i){
+    scanf("%s",pos);
+    int y = pos[0]-'a';
+    int x = pos[1]-'1';
+    int p = x*C+y;
+    cout<<str[p]<<endl;
+  }
 }
