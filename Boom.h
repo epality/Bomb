c62943840cae1fbc43d91f0108402cddbf44daaa
Veritas <mailoflyk@163.com>
2025-05-20 05:04:42 +0800

    gitignore and modify solve.cpp

--- Boom.h
+++ Boom.h
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
 #pragma once
+using namespace std;
 const int PLANE_NUM = 3;
 const int PLANE_SIZE = 10;
 const int R = 10, C = 10;
 const int Up = 0, Right = 1, Down = 2, Left = 3;
 int turnright(int face) {return (face+1)&3;}
 
-typedef std::pair<int,int> Pos;
+typedef pair<int,int> Pos;
 Pos operator+(const Pos &a, const Pos &b) {
-  return std::make_pair(a.first+b.first, a.second+b.second);
+  return make_pair(a.first+b.first, a.second+b.second);
 }
 Pos operator-(const Pos &a, const Pos &b) {
-  return std::make_pair(a.first-b.first, a.second-b.second);
+  return make_pair(a.first-b.first, a.second-b.second);
 }
 Pos operator*(const int &a, const Pos &b) {
-  return std::make_pair(a*b.first, a*b.second);
+  return make_pair(a*b.first, a*b.second);
 }
 Pos operator*(const Pos &b, const int &a) {
-  return std::make_pair(a*b.first, a*b.second);
+  return make_pair(a*b.first, a*b.second);
 }
 
 const Pos v[] = {{-1,0}, {0,1}, {1,0}, {0,-1}};
 const Pos PlaneShape[PLANE_SIZE] = {{0,0}, {0,-1}, {1,-1}, {2,-1}, {-1,-1}, {-2,-1}, {0,-2}, {0,-3}, {1,-3}, {-1,-3}};
 
-std::vector<std::array<char, R*C> > ans_list;
+vector<array<char, R*C> > ans_list;
 const int ANS_NUM = 66816;
 void get_ans_list(){
   freopen("AnswerList.txt","r",stdin);
   char str[R*C];
-  std::array<char, R*C> arr;
+  array<char, R*C> arr;
   while(scanf("%s",str)!=EOF){
     for(int i=0;i<R*C;++i) arr[i]=str[i]-'0';
     ans_list.push_back(arr);
@@ -38,8 +39,8 @@ void get_ans_list(){
   freopen("CON","r",stdin);
 }
 
-std::bitset<ANS_NUM> eigen[R*C][3];
-void count_ans(int ans_id, std::array<char,R*C> ans){
+bitset<ANS_NUM> eigen[R*C][3];
+void count_ans(int ans_id, array<char,R*C> ans){
   for(int i=0;i<R*C;++i){
     eigen[i][ans[i]][ans_id]=1;
   }
@@ -47,4 +48,31 @@ void count_ans(int ans_id, std::array<char,R*C> ans){
 void get_choice_vector(){
   get_ans_list();
   for(int i=0;i<ANS_NUM;++i) count_ans(i,ans_list[i]);
+}
+
+static const double K = 0.79;
+double get_weight(bitset<ANS_NUM> v0,bitset<ANS_NUM> v1,bitset<ANS_NUM> v2){
+  double res = 0;
+  int x0 = v0.count(), x1 = v1.count(), x2 = v2.count();
+  // cout<<x0<<" "<<x1<<" "<<x2<<endl;
+  if(x0) res += x0*log(x0);
+  if(x1) res += x1*log(x1);
+  if(x2) res += x2*log(x2);
+  res /= x0+x1+x2;
+  return -res+x2/(x0+x1+x2)*K;
+}
+
+int solve(bitset<ANS_NUM> cur, bitset<R*C> ban){
+  int best_i = -1;
+  double best_w = -1e9;
+  for(int i=0;i<R*C;++i){
+    if(ban[i]) continue;
+    double w = get_weight(eigen[i][0]&cur,eigen[i][1]&cur,eigen[i][2]&cur);
+    // cout<<i<<" "<<w<<endl;
+    if(best_i==-1||w>best_w){
+      best_i = i;
+      best_w = w;
+    }
+  }
+  return best_i;
 }
--- solve.cpp
+++ solve.cpp
@@ -1,40 +1,16 @@
 #include "Boom.h"
 using namespace std;
 
-static const double K = 0.79;
-double get_weight(bitset<ANS_NUM> v0,bitset<ANS_NUM> v1,bitset<ANS_NUM> v2){
-  double res = 0;
-  int x0 = v0.count(), x1 = v1.count(), x2 = v2.count();
-  // cout<<x0<<" "<<x1<<" "<<x2<<endl;
-  if(x0) res += x0*log(x0);
-  if(x1) res += x1*log(x1);
-  if(x2) res += x2*log(x2);
-  res /= x0+x1+x2;
-  return -res+x2/(x0+x1+x2)*K;
-}
-
-int solve(bitset<ANS_NUM> cur){
-  int best_i = -1;
-  double best_w = -1e9;
-  for(int i=0;i<R*C;++i){
-    double w = get_weight(eigen[i][0]&cur,eigen[i][1]&cur,eigen[i][2]&cur);
-    // cout<<i<<" "<<w<<endl;
-    if(best_i==-1||w>best_w){
-      best_i = i;
-      best_w = w;
-    }
-  }
-  return best_i;
-}
-
 int main(){
   get_choice_vector();
   bitset<ANS_NUM> cur;
+  bitset<R*C> vis;
   cur.set();
   int kill = 0;
   for(int i=0;i<14;++i){
     // puts("!!!!!!!!");
-    int x = solve(cur), y;
+    int x = solve(cur,vis), y;
+    vis[x]=true;
     cout<<(char)((x%10)+'a')<<x/10+1<<endl;
     cin>>y;
     if(y==2) ++kill;
